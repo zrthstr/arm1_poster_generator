e0a7cdb7bd8820a4657255a3df81f18b045c2a6c
User <User@CR00259.local>
2017-03-18 15:26:17 +0100

    added osx options

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
+## linux
 chromium --disable-web-security --user-data-dir=.  'ARM1 Gate-level Simulation.html'
+
+## osx
+## broken.. does not care about args..
+#open -a "Google Chrome" --args  --disable-web-security --user-data-dir=.  'ARM1 Gate-level Simulation.html'
+# this works
+sascript start_chrome_osx.scpt
+
